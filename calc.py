2b1ed71a6f4bd00e9cfc30e76f054ec881903a62
J.R. Leeman <kd5wxb@gmail.com>
2019-11-11 15:57:32 -0600

    Add density calculation.

--- calc.py
+++ calc.py
@@ -5,3 +5,9 @@ def acoustic_impedance(rho, vp):
   Calculate acoustic impedance of a rock.
   """
   return rho * vp
+
+def density(mass, volume):
+    """
+    Calculate density.
+    """
+    return mass / volume
